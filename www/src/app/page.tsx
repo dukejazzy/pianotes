b76d92fdf2ed70b02f884810e79c4581612647da
Jeffrey Desir <jeffrey@desir.tech>
2023-08-02 17:49:55 -0400

    re

--- www/src/app/page.tsx
+++ www/src/app/page.tsx
@@ -2,112 +2,214 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section className="relative">
+    <img
+      className="absolute left-0 top-0"
+      src="flaro-assets/images/headers/gradient.svg"
+      alt=""
+    />
+    <img
+      className="absolute right-0 bottom-0"
+      src="flaro-assets/images/headers/gradient2.svg"
+      alt=""
+    />
+    <div className="container mx-auto overflow-hidden">
+      <div className="relative z-20 flex items-center justify-between px-4 py-5 bg-transparent">
+        <div className="w-auto">
+          <div className="flex flex-wrap items-center">
+            <div className="w-auto mr-14">
+              <a href="#">
+                <img src="flaro-assets/logos/flaro-logo-black.svg" alt="" />
+              </a>
+            </div>
+            <div className="w-auto hidden lg:block">
+              <ul className="flex items-center mr-16">
+                <li className="mr-9 font-medium hover:text-gray-700">
+                  <a href="#">Features</a>
+                </li>
+                <li className="mr-9 font-medium hover:text-gray-700">
+                  <a href="#">Solutions</a>
+                </li>
+                <li className="mr-9 font-medium hover:text-gray-700">
+                  <a href="#">Resources</a>
+                </li>
+                <li className="font-medium hover:text-gray-700">
+                  <a href="#">Pricing</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="w-auto">
+          <div className="flex flex-wrap items-center">
+            <div className="w-auto hidden lg:block">
+              <div className="inline-block">
+                <button
+                  className="py-2.5 px-4 text-base w-full font-medium border border-gray-400 hover:border-gray-500 rounded-xl focus:ring focus:ring-gray-50 bg-white hover:bg-gray-50 transition ease-in-out duration-200"
+                  type="button"
+                >
+                  Start 14 Days Free Trial
+                </button>
+              </div>
+            </div>
+            <div className="w-auto lg:hidden">
+              <a href="#">
+                <svg
+                  className="navbar-burger text-indigo-600"
+                  width={51}
+                  height={51}
+                  viewBox="0 0 56 56"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect width={56} height={56} rx={28} fill="currentColor" />
+                  <path
+                    d="M37 32H19M37 24H19"
+                    stroke="white"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="hidden navbar-menu fixed top-0 left-0 bottom-0 w-4/6 sm:max-w-xs z-50">
+        <div className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-80" />
+        <nav className="relative z-10 px-9 pt-8 bg-white h-full overflow-y-auto">
+          <div className="flex flex-wrap justify-between h-full">
+            <div className="w-full">
+              <div className="flex items-center justify-between -m-2">
+                <div className="w-auto p-2">
+                  <a className="inline-block" href="#">
+                    <img src="flaro-assets/logos/flaro-logo-black.svg" alt="" />
+                  </a>
+                </div>
+                <div className="w-auto p-2">
+                  <a className="navbar-burger" href="#">
+                    <svg
+                      width={24}
+                      height={24}
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M6 18L18 6M6 6L18 18"
+                        stroke="#111827"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col justify-center py-16 w-full">
+              <ul>
+                <li className="mb-12">
+                  <a className="font-medium hover:text-gray-700" href="#">
+                    Features
+                  </a>
+                </li>
+                <li className="mb-12">
+                  <a className="font-medium hover:text-gray-700" href="#">
+                    Solutions
+                  </a>
+                </li>
+                <li className="mb-12">
+                  <a className="font-medium hover:text-gray-700" href="#">
+                    Resources
+                  </a>
+                </li>
+                <li>
+                  <a className="font-medium hover:text-gray-700" href="#">
+                    Pricing
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="flex flex-col justify-end w-full pb-8">
+              <div className="flex flex-wrap">
+                <div className="w-full">
+                  <div className="block">
+                    <button
+                      className="py-2.5 px-4 text-base w-full font-medium border border-gray-400 hover:border-gray-500 rounded-xl focus:ring focus:ring-gray-50 bg-white hover:bg-gray-50 transition ease-in-out duration-200"
+                      type="button"
+                    >
+                      Start 14 Days Free Trial
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+    </div>
+    <div className="relative z-10 overflow-hidden pt-16">
+      <div className="container px-4 mx-auto">
+        <div className="text-center">
+          <p className="mb-5 text-sm text-indigo-600 font-semibold uppercase tracking-px">
+            👋 Meet new Flaro
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <h1 className="mb-9 text-6xl md:text-8xl xl:text-10xl font-bold font-heading tracking-px-n leading-none">
+            The Ultimate Webinar Tool
+          </h1>
+          <div className="mb-7 md:inline-block">
+            <button
+              className="py-4 px-6 w-full text-white font-semibold border border-indigo-700 rounded-xl focus:ring focus:ring-indigo-300 bg-indigo-600 hover:bg-indigo-700 transition ease-in-out duration-200"
+              type="button"
+            >
+              Start 14 Days Free Trial
+            </button>
+          </div>
+          <div className="mb-16 block">
+            <a className="inline-block text-black hover:text-gray-800" href="#">
+              <div className="flex flex-wrap items-center -m-1.5">
+                <div className="w-auto p-1.5">
+                  <svg
+                    width={11}
+                    height={13}
+                    viewBox="0 0 11 13"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M4.18003 11.4261C2.8586 12.3918 1 11.448 1 9.81135V3.43865C1 1.80198 2.8586 0.858209 4.18003 1.82387L8.5403 5.01022C9.63359 5.80916 9.63359 7.44084 8.5403 8.23978L4.18003 11.4261Z"
+                      stroke="currentColor"
+                      strokeWidth="1.5"
+                      strokeMiterlimit={10}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </div>
+                <div className="w-auto p-1.5">
+                  <p className="font-medium">See how the magic happens</p>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div className="relative max-w-max mx-auto">
+            <img
+              className="mx-auto transform hover:scale-105 transition ease-in-out duration-1000"
+              src="flaro-assets/images/headers/dashboard.png"
+              alt=""
+            />
+            <img
+              className="hidden xl:block absolute -left-52 top-16"
+              src="flaro-assets/images/headers/card.png"
+              alt=""
+            />
+          </div>
+        </div>
       </div>
-    </main>
+    </div>
+  </section>
+  
   )
 }
